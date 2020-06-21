4c5c007a2bf928e418f20e50d51af64c38d521a8
vscav <vscavinner@gmail.com>
2020-06-22 01:25:18 +0200

    StepButton component and Machine logic

--- src/components/Machine/Machine.vue
+++ src/components/Machine/Machine.vue
@@ -1,10 +1,53 @@
 <template>
-  <h1>Machine component</h1>
+  <div class="machine">
+    <h1>Machine component</h1>
+    <div v-for="(drum, i) in drums" :key="i">
+      <step-button
+        v-for="(step, j) in stepCount"
+        :key="`drumCount-${j}`"
+        :value="pattern[i][j].active"
+        :highlight="currentStep == j"
+        @input="(val) => (pattern[i][j].active = val)"
+      ></step-button>
+    </div>
+  </div>
 </template>
 <script lang="ts">
-import { Vue } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
+import StepButton from "@/components/Machine/StepButton/StepButton.vue";
 
-export default class Machine extends Vue {}
+@Component({
+  components: {
+    StepButton,
+  },
+})
+export default class Machine extends Vue {
+  private drums: Array<{ fileName: string }> = [];
+  private stepCount = 16;
+  private currentStep = 0;
+  private mutes: Array<boolean> = [];
+  private pattern: Array<Array<{ active: boolean }>> = [];
+
+  created() {
+    for (let i = 0; i < 4; i += 1) {
+      this.drums.push({
+        fileName: "fileName.wav",
+      });
+      this.mutes.push(false);
+    }
+
+    for (let i = 0; i < this.drumsCount; i += 1) {
+      this.pattern.push([]);
+      for (let j = 0; j < this.stepCount; j += 1) {
+        this.pattern[i].push({ active: false });
+      }
+    }
+  }
+
+  get drumsCount() {
+    return this.drums.length;
+  }
+}
 </script>
 
 <style></style>
---
+++ src/components/Machine/StepButton/StepButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <span
+    class="stepbutton"
+    :class="{ active: value, highlight: highlight }"
+    @click="changeStatus"
+  >
+  </span>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class StepButton extends Vue {
+  @Prop({ required: true, type: Boolean, default: false })
+  readonly value!: boolean;
+  @Prop({ required: true, type: Boolean, default: false })
+  readonly highlight!: boolean;
+
+  public changeStatus(): void {
+    this.$emit("input", !this.value);
+  }
+}
+</script>
+
+<style>
+.stepbutton {
+  display: inline-block;
+  width: 32px;
+  height: 32px;
+  border-radius: 7px;
+  border: 0px solid rgba(0, 0, 0, 0.5);
+  border-width: 0px 1px 4px 1px;
+  box-shadow: 0px 5px 1px 1px rgba(0, 0, 0, 0.3),
+    inset 0px 0px 0px 3px rgba(0, 0, 0, 0.1);
+  background-color: #cdd7b8;
+  cursor: pointer;
+  margin: 16px 3px;
+  transition: all 0.1s ease-in-out;
+}
+.stepbutton.active {
+  border-bottom-width: 1px;
+  box-shadow: 0px 1px 0px 1px rgba(0, 0, 0, 0.3),
+    inset 0px 0px 0px 3px rgba(0, 0, 0, 0.1);
+  background-color: #bac2ab;
+}
+</style>
