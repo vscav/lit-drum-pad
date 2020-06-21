fd9d7ee3b74018bad8d183fe3b277d75a24a9d71
vscav <vscavinner@gmail.com>
2020-06-22 00:07:47 +0200

    Machine component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Machine />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Machine from "./components/Machine/Machine.vue";
 
 @Component({
   components: {
-    HelloWorld
-  }
+    Machine,
+  },
 })
 export default class App extends Vue {}
 </script>
---
+++ src/components/Machine/Machine.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Machine component</h1>
+</template>
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+
+export default class Machine extends Vue {}
+</script>
+
+<style></style>
